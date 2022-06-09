b2bfa1aae27d2b75c14f5fef581d86305bebf8ab
Hareem Nadeem <h.nadeem@student.unsw.edu.au>
2022-06-09 17:14:14 +1000

    shout with a number

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -96,8 +96,10 @@ def shout_with_a_number(a_string, a_number):
           challenge, see if you can make the test pass with at least two ways
           of doing the same job.
     """
-    the_answer = shout(a_string) +
+    #the_answer = shout(a_string) +" "+ str(a_number)
+    the_answer = f"{shout(a_string)} {a_number}"
     return the_answer
+
 
 
 """#################################
