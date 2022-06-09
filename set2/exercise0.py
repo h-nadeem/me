c9c9d0585f5ebb97fafdef2e6ced6d881aee073c
Hareem Nadeem <h.nadeem@student.unsw.edu.au>
2022-06-09 16:31:44 +1000

    add 1

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -42,16 +42,17 @@ def add_5(a_number):
         return a_number plus five
     except expressed in python, not english
     """
-    the_answer = None
+    the_answer = a_number + 5
     return the_answer
 
 
 def adder(a_number, another_number):
     """Add two numbers.
-
+    
     Same as above, but with any two numbers.
     """
-    the_answer = None
+    
+    the_answer = a_number + another_number
     return the_answer
 
 
@@ -66,7 +67,7 @@ def shout(a_string):
           they all do different things. You'll need to actually read the
           docs to find out which one you actually need.
     """
-    the_answer = None
+    the_answer = a_string.upper()
     return the_answer
 
 
@@ -79,7 +80,7 @@ def really_shout(a_string):
     that you've reused the function you already wrote.
     Look up how to 'concatinate' strings to make this happen.
     """
-    the_answer = None
+    the_answer = shout(a_string + "!") 
     return the_answer
 
 
@@ -95,7 +96,7 @@ def shout_with_a_number(a_string, a_number):
           challenge, see if you can make the test pass with at least two ways
           of doing the same job.
     """
-    the_answer = None
+    the_answer = shout(a_string) +
     return the_answer
 
 
