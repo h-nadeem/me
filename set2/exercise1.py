e9bbda9fac6d83daac88bc37dde41e59b0bcd418
Hareem Nadeem <h.nadeem@student.unsw.edu.au>
2022-06-09 16:05:01 +1000

    python ../course/set1/tests.py

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -15,20 +15,24 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
+# I think because of the square brackets it is defining a list and creating an index for everything in ''
 some_words = ["what", "does", "this", "line", "do", "?"]
 
+# finding 'word' in the function some_words & printing it
 for word in some_words:
     print(word)
 
+# for loop, it repeats x infinitely times and prints it 
 for x in some_words:
     print(x)
-
+# prints both x and word in the function some_Words
 print(some_words)
 
+# the length of some_words is greater than 3 than print 'some_words contains more than 3 words'
 if len(some_words) > 3:
     print("some_words contains more than 3 words")
 
-
+# defines usefulfunction and prints the name and useful information of platform
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
@@ -37,5 +41,5 @@ def usefulFunction():
     """
     print(platform.uname())
 
-
+#runs function
 usefulFunction()
